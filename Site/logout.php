e880a50b0823a234627d16cb02163248ae9e9150
Tmerckens <tmerckens@student.tudelft.nl>
2015-04-05 16:58:04 +0200

    Added php files Ordened file system site

---
+++ Site/logout.php
@@ -0,0 +1,7 @@
+<?php 
+ $past = time() - 100; 
+ //this makes the time in the past to destroy the cookie 
+ setcookie('name', gone, $past); 
+ setcookie('pass', gone, $past); 
+ header("Location: login.php"); 
+ ?> 
